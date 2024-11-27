d05fd24dfb9b2a8db0e0941bb6d7b0dbab71303d
Leonard-S-S <Kk_cj@outlook.com>
2024-11-27 01:03:42 -0600

    implementación de verificación de tipos con operaciones (falla con paréntesis)

--- output.cpp
+++ output.cpp
@@ -2,21 +2,7 @@
 using namespace std;
 
 int main() {
-string nombre ;
 int x ;
-int i ;
-bool y = false ;
-string xd ;
-xd="holam";cout << "Programa Ejemplo" << endl;
-for (i = 0; i <= 10; i = i + 1) {
-cout << "Hola Mundo número: " << i << endl;
-}
-cout << "Dame tu nombre" << endl;
-cin >> nombre;
-cout << "Cuántas veces quieres que se imprima?" << endl;
-cin >> x;
-for (i = 0; i < x; i = i + 1) {
-cout << nombre << " " << i << endl;
-}
-    return 0;
+float y ;
+y=(2 * 6) + (1 - 2) / 1 + 2;    return 0;
 }
